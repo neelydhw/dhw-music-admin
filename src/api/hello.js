4ac778c0cd3d19c6d9a55a76b385fa20c8f12196
dhw <neelydhw@hotmail.com>
2023-03-22 18:52:41 +0800

    测试API

---
+++ src/api/hello.js
@@ -0,0 +1,4 @@
+import {get} from "./request.js";
+export const sayHello = () =>{
+    return get("/hello");
+}
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,28 @@
 <script setup>
 import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
+import {sayHello} from "../api/hello.js";
 
 const count = ref(0)
+const message = ref("胖丁音乐盒后台")
+const hi = () =>{
+  sayHello().then(res=>{
+    message.value = res.data;
+  })
+
+
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <h1>{{ message }}</h1>
+  <q-btn color="primary" @click="count++">点击次数{{count}}</q-btn>
+  <br>
+  <br>
+  <q-btn color="primary" @click="hi">请求接口</q-btn>
 </template>
 
 <style scoped>
-.read-the-docs {
+a {
   color: #888;
 }
 </style>
